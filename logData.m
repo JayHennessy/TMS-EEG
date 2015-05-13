5f31d97f1a98f7ff79dba8efe1135104becc2b2f
Jay Hennessy <tjay.hennessy@gamil.com>
2015-05-13 16:29:48 -0400

    started a pure fieldtrip pipeline

--- logData.m
+++ logData.m
@@ -1,11 +1,11 @@
-function [ ] = logData( title, type, chanNumStart, chanNumEnd, numEpochs, numEpochsRmv, cutoff, numComp, numCompRmv, numEpochsRmv2)
+function [ ] = logData( title, chanNumStart, chanNumEnd, numEpochs, numEpochsRmv, cutoff, numComp, numCompRmv, numEpochsRmv2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 cd('C:\Users\jay\Desktop\Work\EEG Data');
 
 fileID = fopen('TMS_EXP_LOG.txt','a');
-fprintf(fileID,'%12s\t %4.1f\t  %4.1f\t    %4.1f\t  %4.1f\t    %4.1f\t    %4.3f\t  %4.1f\t   %4.1f\t       %4.1f\t      %12s\t\n',date, type, chanNumStart, chanNumEnd, numEpochs, numEpochsRmv, cutoff, numComp, numCompRmv, numEpochsRmv2 ,title);
+fprintf(fileID,'%12s\t %4.1f\t    %4.1f\t  %4.1f\t    %4.1f\t    %4.3f\t  %4.1f\t   %4.1f\t       %4.1f\t      %12s\t\n',date, chanNumStart, chanNumEnd, numEpochs, numEpochsRmv, cutoff, numComp, numCompRmv, numEpochsRmv2 ,title);
 fclose(fileID);
 end
 
